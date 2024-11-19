37ea244b0af1319d86047fe889240a0509d0e4e9
simjae97 <142781624+simjae97@users.noreply.github.com>
2024-11-19 20:30:16 +0900

    1234

--- src/Application.java
+++ src/Application.java
@@ -2,11 +2,12 @@ import model.dao.EmployeeDao;
 import model.dto.EmployeeDTO;
 import view.MainView;
 
-import java.util.Scanner;
+
 
 public class Application {
     public static void main(String[] args) {
         new MainView().run();
+
     }
 }
 
