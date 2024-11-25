b969c057f928529e653f404bf01d6b843f0dda7b
simjae97 <142781624+simjae97@users.noreply.github.com>
2024-11-25 20:31:54 +0900

    새프로젝트 안열러서 여기에 jsonparsing업로드

---
+++ src/jsonparsing/test.js
@@ -0,0 +1,190 @@
+function topMenu() {
+	document.write("<header>");
+	document.write("<h1 class='logo'>");
+	document.write("<a href=''><span class='txt-blind'>LOGO</span></a>");
+	document.write("</h1>");
+	document.write("<div class='user-info-box'>");
+	document.write("<span class='grade'>관리자</span>");
+	document.write("<div class='drop-menu'>");
+	document.write("<button type='button' class='btn-drop'>홍길순(admin5678)</button>");
+	document.write("<ul class='drop-btn-list'>");
+	document.write("<li><button type='button'>나의 정보</button></li>");
+	document.write("<li><button type='button'>로그아웃</button></li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</div>");
+	document.write("</header>");
+	document.write("<div class='container'>");
+	document.write("<div class='gnb-wrap'>");
+	document.write("<ul class='gnb'>");
+	document.write("<li>");
+	document.write("<a class='btn-menu message'>메시지</a>");
+	document.write("<div class='dep2'>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li><a class='btn-submenu'>메시지 발송</a></li>");
+	document.write("<li><a class='btn-submenu'>임시 보관함</a></li>");
+	document.write("<li>");
+	document.write("<a class='btn-submenu has-sub'>메시지 승인함</a>");
+	document.write("<div class='dep3'>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li><a class='btn-submenu'>승인 요청함</a></li>");
+	document.write("<li><a class='btn-submenu'>승인 대기함</a></li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</li>");
+	document.write("<li>");
+	document.write("<a class='btn-menu campain'>캠페인</a>");
+	document.write("<div class='dep2'>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li><a class='btn-submenu'>캠페인 설정</a></li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</li>");
+	document.write("<li>");
+	document.write("<a class='btn-menu push-current'>발송현황</a>");
+	document.write("<div class='dep2'>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li><a class='btn-submenu'>발송 현황</a></li>");
+	document.write("<li><a class='btn-submenu'>예약 발송현황</a></li>");
+	document.write("<li><a class='btn-submenu'>실시간 발송현황</a></li>");
+	document.write("<li><a class='btn-submenu'>발송이력 조회</a></li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</li>");
+	document.write("<li>");
+	document.write("<a class='btn-menu push-target'>발송대상</a>");
+	document.write("<div class='dep2'>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li><a class='btn-submenu'>테스트 그룹 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>세그먼트 그룹 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>수신거부 유저 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>수신자 그룹 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>푸시 등록 회원 관리</a></li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</li>");
+	document.write("<li>");
+	document.write("<a class='btn-menu template'>템플릿</a>");
+	document.write("<div class='dep2'>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li><a class='btn-submenu'>(일반) ALL 템플릿 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>(검수) 알림톡 템플릿 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>(검수) 네이버톡 템플릿 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>(검수) RCS 템플릿 관리</a></li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</li>");
+	document.write("<li>");
+	document.write("<a class='btn-menu statistics'>통계</a>");
+	document.write("<div class='dep2'>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li><a class='btn-submenu'>앱 가입 통계</a></li>");
+	document.write("<li><a class='btn-submenu'>채널별 발송 통계</a></li>");
+	document.write("<li><a class='btn-submenu'>현황 통계</a></li>");
+	document.write("<li><a class='btn-submenu'>A/B 테스트 통계</a></li>");
+	document.write("<li><a class='btn-submenu'>캠페인 통계</a></li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</li>");
+	document.write("<li>");
+	document.write("<a class='btn-menu setting'>설정</a>");
+	document.write("<div class='dep2'>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li>");
+	document.write("<a class='btn-submenu has-sub'>계정/권한 관리</a>");
+	document.write("<div class='dep3'>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li><a class='btn-submenu'>관리자 계정 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>메뉴 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>승인 그룹 관리</a></li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</li>");
+	document.write("<li>");
+	document.write("<a class='btn-submenu has-sub'>사용자 관리</a>");
+	document.write("<div class='dep3'>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li><a class='btn-submenu'>조직도 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>사용자 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>로그인 기록</a></li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</li>");
+	document.write("<li>");
+	document.write("<a class='btn-submenu has-sub'>채널 관리</a>");
+	document.write("<div class='dep3'>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li><a class='btn-submenu'>채널 사용 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>문자 발신번호 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>RCS 브랜드 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>카카오톡 채널 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>네이버톡 관리</a></li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</li>");
+	document.write("<li>");
+	document.write("<a class='btn-submenu has-sub'>메시지 발송 관리</a>");
+	document.write("<div class='dep3'>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li><a class='btn-submenu'>발송 우선 순위 설정</a></li>");
+	document.write("<li><a class='btn-submenu'>고객피로도 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>중계사 발송 비율 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>금칙어/필수어 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>치환코드 예시 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>수신거부 메시지 관리</a></li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</li>");
+	document.write("<li>");
+	document.write("<a class='btn-submenu has-sub'>시스템 설정</a>");
+	document.write("<div class='dep3 '>");
+	document.write("<ul class='list-submenu'>");
+	document.write("<li><a class='btn-submenu'>공지사항 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>비밀번호 정책 관리</a></li>");
+	document.write("<li><a class='btn-submenu'>개인정보 다운로드 기록</a></li>");
+	document.write("<li><a class='btn-submenu'>공통 코드 관리</a></li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</li>");
+	document.write("</ul>");
+	document.write("</div>");
+	document.write("</div>");
+	
+	
+}
+
+topMenu();
+
+document.addEventListener("DOMContentLoaded", function() {
+    // 모든 메뉴를 가져오기
+    const menuItems = document.querySelectorAll('.gnb > li');
+
+    menuItems.forEach(menuItem => {
+        const menuButton = menuItem.querySelector('.btn-menu');
+        const subMenu = menuItem.querySelector('.dep2');
+
+        if (menuButton && subMenu) {
+            // 메뉴 버튼 클릭 시
+            menuButton.addEventListener('click', function(e) {
+                e.preventDefault(); // 링크 기본 동작 방지
+
+                // 현재 클릭한 메뉴의 서브메뉴 토글
+                if (subMenu.style.display === 'block') {
+                    subMenu.style.display = 'none';
+                } else {
+                    // 다른 메뉴는 닫기
+                    menuItems.forEach(otherItem => {
+                        const otherSubMenu = otherItem.querySelector('.dep2');
+                        if (otherSubMenu && otherSubMenu !== subMenu) {
+                            otherSubMenu.style.display = 'none';
+                        }
+                    });
+                    subMenu.style.display = 'block';
+                }
+            });
+        }
+    });
+});
